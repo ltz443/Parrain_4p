8d2f58426bed4ff9b8799c74e00e678c083bca2c
ltz443 <tonnaleo@gmail.com>
2026-04-27 06:54:38 +0200

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'v1_cache';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/icons/icon-192x192.png'
+];
+
+// Installation : Mise en cache des fichiers
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Stratégie Cache-First : On pioche dans le cache, sinon on va sur le réseau
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});

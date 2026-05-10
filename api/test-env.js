a38e77ba089c35cd6a76cca2a0ade44ee6cd1f1f
ltz443 <tonnaleo@gmail.com>
2026-05-10 03:29:57 +0200

    Delete test-env.js

--- api/test-env.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    hasResendKey: !!process.env.RESEND_API_KEY,
-    resendKeyPrefix: process.env.RESEND_API_KEY ? process.env.RESEND_API_KEY.substring(0, 5) : 'none',
-    hasSupabaseKey: !!process.env.SUPABASE_SERVICE_ROLE_KEY
-  });
-}
